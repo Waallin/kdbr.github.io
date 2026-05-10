977ba6e7c337ba2265d8481e56e605d06cf98f57
ViktorW <viktor.wallin@visma.com>
2026-05-10 13:25:15 +0200

    Update dashboard

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -12,7 +12,7 @@ setLogLevel("silent"); // Tysta loggnivån
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfh94gHvxpHWlMq-8yzjp-2dfd-nXibjM",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "kudoo-36de9.firebaseapp.com",
   projectId: "kudoo-36de9",
   storageBucket: "kudoo-36de9.firebasestorage.app",
